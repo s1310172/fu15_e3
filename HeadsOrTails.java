b1cc67abf26aac6aebcc0a3b28d2c814f0305e9d
s1310172 <s1310172@u-aizu.ac.jp>
2024-07-02 14:47:22 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+class HeadsOrTails{
+  public static void main(String[] args){
+    Random rnd = new Random();
+    String str;
+    int heads = 0, tails = 0;
+
+    System.out.println("Tossing a coin...");
+    for(int i = 1; i <= 3; i++){
+      if(rnd.nextBoolean()){
+        str = "Heads";
+        heads++;
+      }
+      else{
+        str = "Tails";
+        tails++;
+      }
+      System.out.println("Round " + i + ": " + str);
+    }
+    System.out.println("Heads: " + heads + ", Tails: " + tails);
+  }
+}
