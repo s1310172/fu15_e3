3b22b1c3e822c1fc2ed275bd407993a837207c48
s1310172 <s1310172@u-aizu.ac.jp>
2024-07-02 15:05:30 +0900

    Final revision

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -26,10 +26,10 @@ class HeadsOrTails{
     }
     System.out.println("Heads: " + heads + ", Tails: " + tails);
     if(heads > tails){
-      System.out.println("You won");
+      System.out.println(name + " won!");
     }
     else{
-      System.out.println("You lost");
+      System.out.println(name + " lost!");
     }
   }
 }
